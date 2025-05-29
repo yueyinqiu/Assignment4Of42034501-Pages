07f1d8384b6ac8d9ad4a38e8c4dde0646364e8f8
yueyinqiu <yueyinqiu@outlook.com>
2025-05-29 16:54:20 +0800

    d

---
+++ JishilouFourthFloor/JishilouFourthFloor/PageMap.razor.cs
@@ -0,0 +1,109 @@
+﻿using LeafletForBlazor;
+using Microsoft.AspNetCore.Components;
+
+namespace JishilouFourthFloor;
+
+partial class PageMap
+{
+    [Parameter]
+    public string? Current { get; set; }
+
+    private RealTimeMap map;
+
+    RealTimeMap.LoadParameters mapParameters = new()
+    {
+        location = new RealTimeMap.Location()
+        {
+            longitude = Data.Rooms.Select(x => x.Value.Longitude).Average(),
+            latitude = Data.Rooms.Select(x => x.Value.Latitude).Average(),
+        },
+        zoomLevel = 19,
+        basemap = new()
+        {
+            basemapLayers = [
+                new ()
+                {
+                    url = "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                    attribution = "©Open Street Map",
+                    title = "Open Street Map",
+                    detectRetina = true,
+                    maxZoom = 19
+                },
+            ]
+        }
+    };
+
+    private void UpdateAppearance()
+    {
+        if (map is null)
+            return;
+
+        map.Geometric.Points.AppearanceOnType(x => x.type != Current).pattern = new RealTimeMap.PointSymbol()
+        {
+            color = "green",
+            fillColor = "green",
+            fillOpacity = 0.5,
+            radius = 10
+        };
+
+        map.Geometric.Points.AppearanceOnType(x => x.type == Current).pattern = new RealTimeMap.PointSymbol()
+        {
+            color = "red",
+            fillColor = "red",
+            fillOpacity = 0.5,
+            radius = 10
+        };
+    }
+
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
+        this.UpdateAppearance();
+    }
+
+    public async Task AddMarks()
+    {
+        foreach (var room in Data.Rooms)
+        {
+            await map.Geometric.Points.add(new RealTimeMap.StreamPoint()
+            {
+                guid = Guid.NewGuid(),
+                latitude = room.Value.Latitude,
+                longitude = room.Value.Longitude,
+                type = room.Key
+            });
+        }
+
+        map.Geometric.Points.AppearanceOnType(_ => true).pattern = new RealTimeMap.PointTooltip()
+        {
+            content = "${type}",
+            opacity = 0.8,
+            permanent = true
+        };
+
+        this.UpdateAppearance();
+    }
+
+    public void OnClick(RealTimeMap.ClicksMapArgs e)
+    {
+        var mouse = new RealTimeMap.StreamPoint()
+        {
+            latitude = e.location.latitude,
+            longitude = e.location.longitude
+        };
+
+        var marks =
+            from mark in e.sender.Geometric.Points.getItems()
+            let distance = e.sender.Geometric.Computations.distance(
+                mark, mouse, RealTimeMap.UnitOfMeasure.meters)
+            where distance < 10
+            orderby distance
+            select mark;
+
+        var result = marks.FirstOrDefault();
+        if (result is null)
+            return;
+
+        NavigationManager.NavigateTo($"/{result.type}");
+    }
+}
---
+++ JishilouFourthFloor/JishilouFourthFloor/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.FluentUI.AspNetCore.Components;
+using JishilouFourthFloor;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddFluentUIComponents(configuration =>
+{
+    configuration.CollocatedJavaScriptQueryString = null;
+});
+
+await builder.Build().RunAsync();
